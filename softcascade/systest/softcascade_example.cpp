07d636a1d927789e12ba628415be6627d3582a47
golden <lujx0301@thundersoft.com>
2014-01-20 21:05:45 +0800

    add code to show detect result

--- softcascade/systest/softcascade_example.cpp
+++ softcascade/systest/softcascade_example.cpp
@@ -30,6 +30,22 @@ int main(int argc, char* argv[])
     Mat roiImg(vec);
     bool isEmpty = roiImg.empty();
     detector.detect(img, roiImg, detection); // note: roiImg is empty.
+    int size = detection.size();
+    if (size > 0){
+        for (int i = 0; i < size; i++){
+	    Detection det = detection[i];
+	    cv::Rect rect(det.x, det.y, det.w, det.h);
+	    cv::Point p0(det.x, det.y);
+	    cv::Point p1(det.x + det.w, det.y + det.h);
+	    rectangle(img, p0, p1, Scalar(0,0,255), 3);
+	}
+	// resize;
+	imshow("detect-face", img);
+	waitKey(0);
+    }
+    else{
+        printf("none face detected\n"); 
+    }
   
 
 
