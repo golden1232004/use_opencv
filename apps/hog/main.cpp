f1061d71d5a150903e84d5359582b53630da7212
golden <golden@mac.com>
2016-06-03 17:09:19 +0800

    add namespace and include

--- apps/hog/main.cpp
+++ apps/hog/main.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/opencv.hpp"
+#include <vector>
 
 using namespace cv;
 
@@ -14,7 +15,7 @@ int main(int argc, char** argv)
     
     trainImg=imread(fn);   //读取图片  
     HOGDescriptor *hog=new HOGDescriptor(cvSize(3,3),cvSize(3,3),cvSize(5,10),cvSize(3,3),9);  //具体意思见参考文章1,2  
-    vector<float> descriptors;//结果数组  
+    std::vector<float> descriptors;//结果数组  
     hog->compute(trainImg, descriptors,Size(1,1), Size(0,0)); //调用计算函数开始计算  
     printf("%d\n",descriptors.size()); //打屏一下结果数组的大小，看看是否符合文献2的预估， 发现完全一样，那篇文章很给力
     int descp = hog->getDescriptorSize();
